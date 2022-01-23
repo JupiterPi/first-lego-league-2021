08c2094654ad67f08900346b9ce49a7bca2b7955
YetiTheWinner <92581685+YetiTheWinner@users.noreply.github.com>
2022-01-23 09:42:06 +0100

    Added $

--- LanesCheck.py
+++ LanesCheck.py
@@ -1,4 +1,4 @@
-#$frame$
+#$content$
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
